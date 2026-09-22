1052713ded5bb190e5e458a8c3b7b37b9af1722f
DavidB <david.bernard@fms-ea.com>
2026-09-22 10:41:17 +0200

    urls account et skills

--- evaldjango/skills_shop/urls.py
+++ evaldjango/skills_shop/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('demande/creer/', views.creer_demande, name='creer_demande'),
+    path('skill/<str:skill_name>/', views.select_skill, name='select_skill'),
 ]
