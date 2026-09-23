9a23ac702296ff258f0856dfdbabae7f51ae492e
DavidB <david.bernard@fms-ea.com>
2026-09-23 09:51:50 +0200

    method index modified to have queries in queries available and accepted queries separately

--- evaldjango/skills_shop/views.py
+++ evaldjango/skills_shop/views.py
@@ -23,12 +23,12 @@ def signup(request):
 def index(request):
     skills = Skill.objects.all()
     if request.user.is_authenticated:
-        available_queries = Query.objects.filter(skill__in=request.user.skills.all())
-        user_queries = Query.objects.filter(user=request.user)
+        available_queries = Query.objects.filter(skill__in=request.user.skills.all(), accepted_by__isnull=True)
+        accepted_queries = Query.objects.filter(accepted_by=request.user)
     else:
         available_queries = []
-        user_queries = []
-    contexte = { 'skills': skills, 'available_queries': available_queries, 'user_queries': user_queries }
+        accepted_queries = []
+    contexte = { 'skills': skills, 'available_queries': available_queries, 'accepted_queries': accepted_queries }
     return render(request, 'index.html', contexte)
 
 @login_required
@@ -43,7 +43,7 @@ def creer_demande(request):
 
 @require_http_methods(["GET"])
 def select_skill(request, skill_name):
-    queries = Query.objects.filter(skill__name=skill_name)
+    queries = Query.objects.filter(skill__name=skill_name, accepted_by__isnull=True)
     contexte = {'skill_name': skill_name, 'queries': queries}
     return render(request, 'slots.html', contexte)
 
@@ -51,6 +51,14 @@ def select_skill(request, skill_name):
 @require_http_methods(["POST"])
 def book_query(request, query_id):
     query = Query.objects.get(id=query_id)
+    skill = query.skill
+
+    if request.user == query.user:
+        return HttpResponse("You cannot book your own query.", status=403)
+
+    if not request.user.skills.filter(pk=skill.pk).exists():
+        return HttpResponse("You must have this skill to book this query.", status=403)
+
     query.accepted_by = request.user
     query.save()
     return redirect('index')
